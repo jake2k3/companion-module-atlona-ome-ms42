09c21f79dd60f0173b9f815e82cf1a3d5cd0d5b7
Jacob Chapman <jacob@chapmansound.com>
2026-08-13 12:30:23 -0500

    initial variables defined

--- src/variables.ts
+++ src/variables.ts
@@ -1,15 +1,39 @@
 import type ModuleInstance from './main.js'
 
 export type VariablesSchema = {
-	variable1: string
-	variable2: string
-	variable3: string
+	input1Connected: string
+	input2Connected: string
+	input3Connected: string
+	input4Connected: string
+	output1Enabled: string
+	output2Enabled: string
+	routeOutput1: string
+	routeOutput2: string
+	statusBlink: string
+	statusLRAUD: string
+	statusPower: string
+	statusUsbHostLogic: string
+	statusUsbHostRoute: string
+	statusUsbVbusControl: string
+	statusVOutMute: string
 }
 
 export function UpdateVariableDefinitions(self: ModuleInstance): void {
 	self.setVariableDefinitions({
-		variable1: { name: 'My first variable' },
-		variable2: { name: 'My second variable' },
-		variable3: { name: 'Another variable' },
+		input1Connected: { name: 'Input 1 Connection Status' },
+		input2Connected: { name: 'Input 2 Connection Status' },
+		input3Connected: { name: 'Input 3 Connection Status' },
+		input4Connected: { name: 'Input 4 Connection Status' },
+		output1Enabled: { name: 'Output 1 Enable Status' },
+		output2Enabled: { name: 'Output 2 Enable Status' },
+		routeOutput1: { name: 'Output 1 Route Status' },
+		routeOutput2: { name: 'Output 2 Route Status' },
+		statusBlink: { name: 'Blink status' },
+		statusLRAUD: { name: 'Analog Audio status' },
+		statusPower: { name: 'Power status' },
+		statusUsbHostLogic: { name: 'USB Host Logic status' },
+		statusUsbHostRoute: { name: 'USB Host Route status' },
+		statusUsbVbusControl: { name: 'USB VBus Power status' },
+		statusVOutMute: { name: 'Output Volume Mute status' },
 	})
 }
